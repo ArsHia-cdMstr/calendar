5fc18743192b60d2cab29c04cf97921508f5330b
arshia.mkh <arshia.mokhlesi@gmail.com>
2022-01-08 12:44:30 +0330

    didn't save the file so the same title of before commit XD

--- main.cpp
+++ main.cpp
@@ -6,15 +6,46 @@
 
 using namespace std ;
 
-void startACalendar()
+void startAGeoCalendar(bool isPersian )
 {
     //find the currunt time
-        time_t now = time(0);
-        tm *ltm = localtime(&now); // currunt time 
+    time_t now = time(0);
+    tm *ltm = localtime(&now); // currunt time 
 
-        int month_1st_weekday = findTheFirstDayOfMonth( ltm ) ;
-        printTheMonthTitle( ltm ) ;
-        
+    int month_1st_weekday = findTheFirstDayOfMonth( ltm ) ;
+    printTheMonthTitle( ltm ) ;
+    printDaysOfAMonth( ltm , month_1st_weekday );
+
+}
+
+void printDaysOfAMonth(tm* time , int M1W)//uncomplete
+{
+    cout << setw((M1W*4)) ;
+
+    for (int i=0 ; i<)
+}
+
+int numberOfDays(int year, int month)
+{
+    switch (month)
+    {
+    case 0: return 31 ;//jan
+    case 1: if (year % 400 == 0 ||      // feb
+                (year % 4 == 0 && year % 100 != 0))
+            return (29);
+        else
+            return (28);
+    case 2: return 31;//march
+    case 3: return 30;//april
+    case 4: return 31;//may
+    case 5: return 30;//june
+    case 6: return 31;//july 
+    case 7: return 31;//Aug
+    case 8: return 30;//Sep
+    case 9: return 31;//Oct
+    case 10: return 30;//Nov
+    case 11: return 31;//Dec
+    }
 }
 
 void printTheMonthTitle(tm* time)
@@ -45,18 +76,26 @@ string monthDayName(int month)
 
 int findTheFirstDayOfMonth(tm *time)
 {
-    return (time->tm_mday * -1)+1 % 7 ;
+    return ((time->tm_wday - ((time->tm_mday - 1) % 7) ) % 7);
 } 
 
-class Calendar 
+class PerCalendar 
 {
 public:
-    Calendar()
+    PerCalendar()
     {
-        startACalendar();
-                       
+        startAPerCalendar();               
     }
-}
+};
+
+class GeoCalendar 
+{
+public:
+    GeoCalendar()
+    {
+        startAGeoCalendar();               
+    }
+};
 
 
 int main()
